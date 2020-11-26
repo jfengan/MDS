c3a9c6ed64f334b8fb1742051e811a916271425e
Jiahao FENG <jiahaofeng@JiahaodeMacBook-Pro.local>
2020-11-26 21:37:42 +0800

    fix_heartbeat_pong_message

--- deribit/deribit_ws.py
+++ deribit/deribit_ws.py
@@ -8,7 +8,7 @@ import logging
 class DeribitRecorder(WSHandler):
     def __init__(self, url, symbols):
         logformatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        log = TimedRotatingFileHandler("Deribit_Tick.log", 'M', 1, 0)
+        log = TimedRotatingFileHandler("Deribit_Tick.log", 'H', 1, 0)
         # log = TimedRotatingFileHandler(filename='Deribit_Tick.log', when='midnight', backupCount=30)
         log.setLevel(logging.INFO)
         log.setFormatter(logformatter)
@@ -32,12 +32,13 @@ class DeribitRecorder(WSHandler):
     @staticmethod
     def get_symbol_channel(symbols: list):
         channels = []
-        for symbol in symbols:
+        # for symbol in symbols:
+        for symbol in ['BTC-PERPETUAL']:
             channels.append("trades.{}.raw".format(symbol))
-            channels.append("book.{}.raw".format(symbol))
+            # channels.append("book.{}.raw".format(symbol))
             if symbol in ['BTC-PERPETUAL', 'ETH-PERPETUAL']:
                 channels.append("perpetual.{}.raw".format(symbol))
-            channels.append("quote.{}".format(symbol))
+            # channels.append("quote.{}".format(symbol))
             channels.append("ticker.{}.raw".format(symbol))
         return channels
 
@@ -46,13 +47,13 @@ class DeribitRecorder(WSHandler):
 
     async def subscribe(self):
         # subscribe index price
-        index_channels = self.get_index_channel(index=self.index_names)
+        """index_channels = self.get_index_channel(index=self.index_names)
         message = {"jsonrpc": "2.0",
                    "method": "public/subscribe",
                    "id": 42,
                    "params": {"channels": index_channels}
                    }
-        await self.ws.send_message([message])
+        await self.ws.send_message([message])"""
 
         # subscribe tickers of symbols
         symbol_channels = self.get_symbol_channel(symbols=self.symbols)
@@ -69,10 +70,10 @@ class DeribitRecorder(WSHandler):
                 {
                     "jsonrpc": "2.0",
                     "id": 9929,
-                    "method": "ping",
+                    "method": "pong",
                 }
             ])
-            await asyncio.sleep(3)
+            await asyncio.sleep(60)
 
 
 if __name__ == "__main__":
@@ -80,7 +81,7 @@ if __name__ == "__main__":
 
     async def t(_loop):
         recorder = DeribitRecorder(url="wss://www.deribit.com/ws/api/v2",
-                                   symbols=_symbols)
+                                   symbols=["BTC-PERPETUAL"])
         await recorder.connect(_loop=_loop)
         asyncio.ensure_future(recorder.process_data(), loop=_loop)
         await recorder.heartbeat_service()
